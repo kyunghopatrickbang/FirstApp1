787624a8b28da91df2e12a2a5df13ea78fad650e
kyunghopatrickbang <2014072830@ust-ics.mygbiz.com>
2017-09-05 23:56:50 +0800

    Initial commit

---
+++ app/src/main/java/com/kyungho/firstapp/ActivityOne.java
@@ -0,0 +1,21 @@
+package com.kyungho.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+
+    public void nextActivity(View view)
+    {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+    }
+}
